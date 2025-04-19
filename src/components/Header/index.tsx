ff802b637ec006da82ae5748a7bff7dbb07e850c
joenog <joabnogueira@outlook.com>
2025-04-19 16:01:58 -0300

    First commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+		<header>
+			<nav className="flex bg-sky-950 justify-between">
+				<div className="flex items-center">
+					<a href="/" className="text-white text-2xl font-bold p-4">
+						soBooks
+					</a>
+				</div>
+				<ul className="flex gap-3 text">
+					<li><a href="/">Home</a></li>
+					<li><a href="/books">Books</a></li>
+
+				</ul>
+			</nav>
+
+		</header>
+	)
+}
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Header";
 
 export function Layout() {
   return(
     <>
+      <Header />
       <Outlet />
     </>
   )
